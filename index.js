c67dffe2c73620a54ef480a0cb5f25bf4a4364ec
Igor Batista <igorsb11@hotmail.com>
2021-03-25 09:42:10 -0300

    Using custom middleware and request params

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const members = require("./Members")
-
 const express = require("express");
 const path = require("path");
+const logger = require("./middleware/logger");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/api/members/", (req,res) => {
-    res.json(members)
-})
+const members = require("./Members");
 
+app.use(logger);
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api/members/", (req, res) => res.json(members));
+
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
