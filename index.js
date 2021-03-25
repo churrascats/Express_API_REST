3a8b973ea1f7bc9d79563b7536b4467097868fc5
Igor Batista <igorsb11@hotmail.com>
2021-03-25 10:14:46 -0300

    Routers implemented

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const path = require("path");
+const PORT = require("./config/port")
 const logger = require("./middleware/logger");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-const members = require("./Members");
 
 app.use(logger);
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/api/members/", (req, res) => res.json(members));
-
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some((member) => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-  }
-});
-
-
+app.use("/api/members", require("./routes/api/member"))
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
